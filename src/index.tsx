48820fdbf90bbc70957499d97b55fdbd1d37369a
Emad-salah <emads14@gmail.com>
2023-04-01 14:31:10 +0000

    Switched storybook API Key

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import AuthArmorSDK from "./components/AuthArmorForm";
 
 render(
   <AuthArmorSDK
-    clientSdkApiKey="eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjhiNzhkYTFmLWU4NWEtNDg4Ny05MmQxLTcyNWZlNTg3MGFiZCJ9.eyJrZXkiOiJXbXhCN1g2TVhGM3BtZEJWUnNIYkVuaHZZN3M1NVVzQkdkTlBiRTdmdkU3VmlSU0wiLCJpYXQiOjE2NTUxNDg5NTh9.d2enMoOLTUeC-9NegdJHn0Xh5AGx8uQ1R68tVAGs0pMTDGf58BCBleeC2w16bmF0hAqui94EcmCX8KSoKhzmiA"
-    webauthnClientId="1bd515ee-c7d5-4e19-ba6e-348f9a785f19"
+    clientSdkApiKey="eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjhiNzhkYTFmLWU4NWEtNDg4Ny05MmQxLTcyNWZlNTg3MGFiZCJ9.eyJrZXkiOiI0U3ZRc2lmRjR4Ykt6RUxhcVVZWGl1eWJ5bkt3Zm5NZFd6aWFOVzc5c3dpa3V3Q3IiLCJpYXQiOjE2ODAzNTY5MTd9.QjwypLZoWVgi2C5P0XaXY-v6mA-wyMhJwtE_L44DTWWJWFwiQo5ECH2UORQ1zZ0Gy7KWMs2_9Am1rCjYZH2-WQ"
+    webauthnClientId="5ceb11dc-3337-44bd-9084-2730d5341f22"
     registerRedirectUrl={`${location.origin}/magic-register`}
     authenticationRedirectUrl={`${location.origin}/magic-login`}
     onAuthSuccess={() => console.log("Login success!")}
