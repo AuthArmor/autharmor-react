ab794f81db10a4266af4f0309257935e55ff90d9
Sara Elsayeda <swe@saraelsa.net>
2024-01-25 10:49:25 -0500

    Fix styling issues

--- src/AuthArmorForm.tsx
+++ src/AuthArmorForm.tsx
@@ -2,11 +2,39 @@ import type {
     AuthArmorFormCustomElementEvents,
     AuthArmorFormCustomElementProps
 } from "@autharmor/autharmor-js-ui";
-import { DetailedHTMLProps, HTMLAttributes } from "react";
-import { wrapWc } from "wc-react";
+import {
+    CSSProperties,
+    ForwardedRef,
+    HTMLAttributes,
+    forwardRef,
+    useImperativeHandle,
+    useLayoutEffect,
+    useRef
+} from "react";
+import { wrapWc } from "./wrapWc";
 
-export type AuthArmorFormProps = AuthArmorFormCustomElementProps &
-    AuthArmorFormCustomElementEvents &
-    DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
+export type AuthArmorFormProps = AuthArmorFormCustomElementProps & AuthArmorFormCustomElementEvents;
 
-export const AuthArmorForm = wrapWc<AuthArmorFormProps>("autharmor-form");
+const AuthArmorFormWc = wrapWc<AuthArmorFormProps>("autharmor-form");
+
+export const AuthArmorForm = forwardRef(
+    (
+        props: AuthArmorFormProps & HTMLAttributes<any>,
+        forwardedRef: ForwardedRef<HTMLElementTagNameMap["autharmor-form"]>
+    ) => {
+        const ref = useRef<HTMLElementTagNameMap["autharmor-form"]>(null);
+        useImperativeHandle(forwardedRef, () => ref.current!, [ref.current]);
+
+        useLayoutEffect(() => {
+            if (ref.current === null) return;
+
+            for (const styleKey in props.style) {
+                (ref.current.style as any)[styleKey as keyof CSSProperties] = (props.style as any)[
+                    styleKey
+                ];
+            }
+        }, [ref.current, props.style]);
+
+        return <AuthArmorFormWc ref={ref} {...props} />;
+    }
+);
